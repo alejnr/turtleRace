0cd9c195840efd2cbbcc6110ec28d87cc3872f50
Alex Menlah <alexamenlah@gmail.com>
2025-01-15 03:07:39 +0000

    giving each turtle a random pace for the race

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make a Bet", prompt="Which turtle will win the race? Enter a colour:")
@@ -13,5 +15,13 @@ for _ in range(0,6):
     new_turtle.penup()
     new_turtle.goto(x=-230, y=y_Positions[_])
     all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
 
 Screen().exitonclick()
